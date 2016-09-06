c8cd547b47682613d4fa2d90d3aca6143e73bb1d
miked <whatthafunk3@aol.com>
2016-09-06 14:28:35 -0500

    finally

--- app/src/main/java/com/bignerdranch/android/tester/MainActivity.java
+++ app/src/main/java/com/bignerdranch/android/tester/MainActivity.java
@@ -2,6 +2,12 @@ package com.bignerdranch.android.tester;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d("Hello", "this is a test");
+
+        Button timeButton = (Button) findViewById(R.id.button);
+        timeButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                String currentTime = new Date().toString();
+                Toast.makeText(MainActivity.this, "The time is " + currentTime, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
